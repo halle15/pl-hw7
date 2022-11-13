06449f83d2ccd456d9f6f37112dc13f398c978e6
ehall <halle15@email.franklin.edu>
2022-11-13 13:11:45 -0500

    comment documentation

--- htoi.c
+++ htoi.c
@@ -2,18 +2,15 @@
 #include <math.h>
 #include "htoi.h"
 
-/*  TODO:
-    1. Fix bug after O------, stackdumps.
-    2. Document literally everything.
-    3. Test edge cases.
-
+/*  equiv
+    Description: takes in relevant hexadecimal character and converts it to integer without using external headers.
+    @param c: The relevant hexadecimal character to be converted.
+    @return int: The integer equivalent of param c.
 
 */
-
-
-
  int equiv(char c){
     switch(c){
+        //Attempted this function with || operator, but it would not work unless i split them and repeated code unfortunately.
         case '0' :
             return 0;
         case '1' :
@@ -58,6 +55,7 @@
             return 15;
         case 'F':
             return 15;
+        //if a non relevant character is entered, this is returned to ensure that a future check will see this as a bad char.
         default :
             return -1;
     }
@@ -65,7 +63,8 @@
  }
 
 /*
-    Author: Evann Hall
+    @param *st: the string array to calculate from
+    @return int: the length of st.    
     Description:
 
     The purpose of this function is to quickly and easily retrieve an integer value for the length of a character array, without using external headers.
@@ -77,12 +76,20 @@ const int len(char *st){
     return x;
 }
 
+/*
+    @param *st: the string array to calculate from
+    @return char: the stripped string array.
+    Description:
+
+    The purpose of this function is to check if there is a OX, or Ox before the input string, and if there is it cuts it off and returns the rest of the hexadecimal
+    using no external header files.
+*/
 const char *convString(char *st){
 
     char first = st[0];
     char second = st[1];
 
-    char total[3] = {first, second};
+    char total[3] = {first, second}; // easy access to the first two characters to check if there is a OX or Ox. It takes up 3 because there needs to be space for the null char.
 
     if(first != 'O') goto el; //no reason for design choice, practiced for a new concept, I find this cleaner than repeating code.
 
@@ -107,7 +114,7 @@ const char *convString(char *st){
     }
     else{
         el: 
-        if(first == 'O') return '\0';
+        if(first == 'O') return '\0'; //returns null string to be checked later to throw error.
         return st;
         
         
@@ -115,6 +122,13 @@ const char *convString(char *st){
 
 }
 
+/*
+    @param *st: the formatted hex string to calculate the integer from.
+    @return int: the calculated integer converted from hex.
+    Description:
+
+    This is the final function that takes the previously formatted input and finally turns it into an integer, using only the math external header file.
+*/
 int htoi(char *st){
     
     int length = len(st);
@@ -135,9 +149,10 @@ int main(){
     char userString[100];
     scanf("%s", userString);
     char* testStr = convString(userString);
+    if(*testStr == '\0'){printf("bad char!"); return -1;}
     int length = len(testStr);
     printf("RESULT: %i", htoi(testStr));
 
-    return 0;
+    return 1;
 }
 
